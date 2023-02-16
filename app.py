f552ae68b96eace165eaaafea5b4d7226a9a3089
Niharika Gupta <niharika.gupta@morningstar.com>
2023-02-15 23:03:38 -0500

    adding logn method and refactor

--- app.py
+++ app.py
@@ -12,25 +12,15 @@ def calculate():
     number_one = request.form["number_one"]
     number_two = request.form["number_two"]
     operation = request.form["operation"]
-
-    if operation == "add":
-        result = float(number_one) + float(number_two)
-        return render_template("calculator.html", result=result)
-
-    elif operation == "subtract":
-        result = float(number_one) - float(number_two)
-        return render_template("calculator.html", result=result)
-
-    elif operation == "multiply":
-        result = float(number_one) * float(number_two)
-        return render_template("calculator.html", result=result)
-
-    elif operation == "divide":
-        result = float(number_one) / float(number_two)
-        return render_template("calculator.html", result=result)
-
+    if(check_if_digt(number_one) and check_if_digt(number_two))
+        result = _math(operation, number_one, number_two)
+        if resul==0:
+            return render_template("calculator.html", result=result)
+        else:
+            return render_template("calculator.html")
     else:
-        return render_template("calculator.html")
+            return render_template("calculator.html")
+
 
 
 @app.errorhandler(404)
@@ -42,6 +32,28 @@ def not_found(error):
 def server_error(error):
     return render_template("500.html", error=error)
 
+def check_if_digt(value):
+    return value.isnumeric()
+
+def _math(operation, num1, num2):
+    match operation {
+        case "add":
+            return  float(num1) + float(num2);
+        case "subtract":
+            result = float(num1) - float(num1);
+            return result;
+        case "multiply":
+            result = float(num1) * float(num2)
+            return result;
+        casee "divide":
+            result = float(num1) / float(num2);
+            return result;
+        case "logn":s
+            result = math.log(float(num1));
+            return result;
+        case _:
+            return 0;
+    }     
 
 if __name__ == "__main__":
     keep_alive()
